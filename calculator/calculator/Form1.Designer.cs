3ed910ea3f99d3d5d7b15f1df2c0d57debed9e66
Adrax <terlychnikita@gmail.com>
2017-06-07 11:56:58 +0500

    Добавил методы генерации контролов

--- calculator/calculator/Form1.Designer.cs
+++ calculator/calculator/Form1.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textC = new System.Windows.Forms.TextBox();
+            this.labelX = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textC
+            // 
+            this.textC.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textC.Location = new System.Drawing.Point(24, 12);
+            this.textC.Margin = new System.Windows.Forms.Padding(0);
+            this.textC.Name = "textC";
+            this.textC.Size = new System.Drawing.Size(38, 23);
+            this.textC.TabIndex = 0;
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.BackColor = System.Drawing.Color.White;
+            this.labelX.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.labelX.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelX.Location = new System.Drawing.Point(62, 15);
+            this.labelX.Margin = new System.Windows.Forms.Padding(0);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(38, 16);
+            this.labelX.TabIndex = 1;
+            this.labelX.Text = "+X1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(518, 259);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.textC);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textC;
+        private System.Windows.Forms.Label labelX;
     }
 }
 
