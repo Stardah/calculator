61f97901b5223dab46e54010a5794b7e440bc58e
Adrax <terlychnikita@gmail.com>
2017-06-07 12:50:49 +0500

    Добавил создание матрицы

--- calculator/calculator/Form1.cs
+++ calculator/calculator/Form1.cs
@@ -15,12 +15,18 @@ namespace calculator
         Font textFont = new Font("Verdana",10.0F,FontStyle.Regular);
         Font labelFont = new Font("Verdana", 10.0F, FontStyle.Bold);
 
-        List<TextBox> boxes = new List<TextBox>();
-        List<Label> labels = new List<Label>();
+        int gapLeft = 38;
+        int gapTop = 38;
+
+        List<List<TextBox>> boxes = new List<List<TextBox>>();
+        List<List<Label>> labels = new List<List<Label>>();
+
 
         public Form1()
         {
             InitializeComponent();
+            StartUp();
+            DrawControls();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -28,23 +34,92 @@ namespace calculator
 
         }
 
-        public void StartUp()
+        /// <summary>
+        /// Отрисовывает все контролы
+        /// </summary>
+        public void DrawControls()
         {
-            
+            foreach (List<TextBox> box in boxes)
+                this.Controls.AddRange(box.ToArray());
+            foreach (List<Label> label in labels)
+                this.Controls.AddRange(label.ToArray());
         }
 
-        public TextBox AddText()
+        /// <summary>
+        /// Отрисовывает на форме контрол
+        /// </summary>
+        /// <param name="control"></param>
+        public void DrawOneControl(Control control)
+        {
+            this.Controls.Add(control);
+        }
+
+        /// <summary>
+        /// Составляет начальную таблицу
+        /// </summary>
+        public void StartUp()
+        {
+            List<TextBox> rowBox;
+            List<Label> rowLabel;
+            int left;
+            int top=10;
+            int prevLeft;
+            Label label;
+            for (int j = 0; j < 3; j++)
+            {
+                rowBox = new List<TextBox>();
+                rowLabel = new List<Label>();
+                left = 10;
+                for (int i = 0; i < 3; i++)
+                {
+                    rowBox.Add(AddBox(left, j * gapTop + top));
+                    if (i == 3-1)
+                        label = AddLabel(left + gapLeft, j * gapTop + top, "X" + (i + 1).ToString() + "=");
+                    else label = AddLabel(left + gapLeft, j * gapTop + top, "X" + (i + 1).ToString() + "+");
+                    rowLabel.Add(label);
+                    left += gapLeft + label.Text.ToString().Length * (int)labelFont.SizeInPoints;
+                }
+
+                rowBox.Add(AddBox(left, j * gapTop + top));
+
+                boxes.Add(rowBox);
+                labels.Add(rowLabel);
+            }
+        }
+
+        /// <summary>
+        /// Создаёт новый textBox
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="top"></param>
+        /// <returns></returns>
+        public TextBox AddBox(int left, int top)
         {
             TextBox textBox = new TextBox();
             textBox.Font = textFont;
+            textBox.Size = new Size(38,23);
+            textBox.Top = top-3;
+            textBox.Left = left;
+            textBox.Text = "0";
             return textBox;
         }
 
-        public Label AddLabel()
+        /// <summary>
+        /// Создаёт новый Label
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="top"></param>
+        /// <returns></returns>
+        public Label AddLabel(int left, int top, string info)
         {
             Label label = new Label();
             label.Font = labelFont;
             label.FlatStyle = FlatStyle.System;
+            label.Size = new Size(38, 16);
+            label.Top = top;
+            label.Left = left;
+            label.Text = info;
+            //label.AutoSize = true;
             return label;
         }
     }
